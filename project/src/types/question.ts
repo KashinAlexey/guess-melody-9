748d71d2f22883c5c9d9991b1c6b0ceaa758d0ed
Alexey <apple230379@gmail.com>
2022-02-22 19:42:41 +0300

    4.2. Добавит моки для игры «Угадай исполнителя». Добавим тестовые данные для игры «Угадай исполнителя». Используем тот же модуль, что и в предыдущем шаге. Также, не забудем описать типы.

--- project/src/types/question.ts
+++ project/src/types/question.ts
@@ -1,10 +1,29 @@
+export type ArtistAnswer = {
+  artist: string;
+  picture: string;
+};
+
+export type Song = {
+  artist: string;
+  src: string;
+};
+
+export type QuestionArtist = {
+  answers: ArtistAnswer[];
+  song: Song;
+  type: string;
+};
+
 export type GenreAnswer = {
   src: string;
   genre: string;
 };
-
 export type QuestionGenre = {
   answers: GenreAnswer[];
   genre: string;
   type: string;
 };
+
+export type Question = QuestionArtist | QuestionGenre;
+
+export type Questions = Question[];
