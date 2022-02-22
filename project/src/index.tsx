865ad14e0a5afd3cfdeba3a77997219d1c494ecf
Alexey <apple230379@gmail.com>
2022-02-22 19:47:38 +0300

    4.3. Подключит моки в файл index.tsx. Подключим модуль с тестовыми данными в файл , а затем передадим данные через пропсы в компонент .

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
+import {questions} from './mocks/questions';
 
 const Setting = {
   ERRORS_COUNT: 3,
 };
-
 ReactDOM.render(
   <React.StrictMode>
     <App
       errorsCount = {Setting.ERRORS_COUNT}
+      questions = {questions}
     />
   </React.StrictMode>,
   document.getElementById('root'));
