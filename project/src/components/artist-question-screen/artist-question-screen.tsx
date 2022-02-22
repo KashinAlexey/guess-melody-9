11b6a09db4888c1ab7fc97059f9b99b70d4b8e80
Alexey <apple230379@gmail.com>
2022-02-22 20:26:51 +0300

    4.7. Обновит компонент ArtistQuestionScreen. WIP Произведём аналогичные действия с компонентом . Стейт нам не потребуется, так как ответ может быть только один.

--- project/src/components/artist-question-screen/artist-question-screen.tsx
+++ project/src/components/artist-question-screen/artist-question-screen.tsx
@@ -1,63 +1,67 @@
+import {ChangeEvent} from 'react';
 import Logo from '../logo/logo';
+import {QuestionArtist, UserArtistQuestionAnswer} from '../../types/question';
 
-function ArtistQuestionScreen(): JSX.Element {
+type ArtistQuestionScreenProps = {
+  question: QuestionArtist,
+  onAnswer: (question: QuestionArtist, answer: UserArtistQuestionAnswer) => void;
+};
+
+function ArtistQuestionScreen(props: ArtistQuestionScreenProps): JSX.Element {
+  const {question, onAnswer} = props;
+  const {answers, song} = question;
+
   return (
     <section className="game game--artist">
       <header className="game__header">
         <Logo />
-
         <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
           <circle className="timer__line" cx="390" cy="390" r="370"
             style={{filter: 'url(#blur)', transform: 'rotate(-90deg) scaleY(-1)', transformOrigin: 'center'}}
           />
         </svg>
-
         <div className="game__mistakes">
           <div className="wrong" />
           <div className="wrong" />
           <div className="wrong" />
         </div>
       </header>
-
       <section className="game__screen">
         <h2 className="game__title">Кто исполняет эту песню?</h2>
         <div className="game__track">
           <div className="track">
             <button className="track__button track__button--play" type="button" />
             <div className="track__status">
-              <audio></audio>
+              <audio
+                src={song.src}
+              />
             </div>
           </div>
         </div>
 
         <form className="game__artist">
-          <div className="artist">
-            <input className="artist__input visually-hidden" type="radio" name="answer" value="artist-1" id="answer-1" />
-            <label className="artist__name" htmlFor="answer-1">
-              <img className="artist__picture" src="img/placeholder.jpg" alt="Пелагея" />
-              Пелагея
-            </label>
-          </div>
-
-          <div className="artist">
-            <input className="artist__input visually-hidden" type="radio" name="answer" value="artist-2" id="answer-2" />
-            <label className="artist__name" htmlFor="answer-2">
-              <img className="artist__picture" src="img/placeholder.jpg" alt="Краснознаменная дивизия имени моей бабушки" />
-              Краснознаменная дивизия имени моей бабушки
-            </label>
-          </div>
-
-          <div className="artist">
-            <input className="artist__input visually-hidden" type="radio" name="answer" value="artist-3" id="answer-3" />
-            <label className="artist__name" htmlFor="answer-3">
-              <img className="artist__picture" src="img/placeholder.jpg" alt="Lordi" />
-              Lordi
-            </label>
-          </div>
+          {answers.map((answer, id) => (
+            <div key={answer.artist} className="artist">
+              <input
+                className="artist__input visually-hidden"
+                type="radio"
+                name="answer"
+                value={`answer-${id}`}
+                id={`answer-${id}`}
+                onChange={(evt: ChangeEvent<HTMLInputElement>) => {
+                  evt.preventDefault();
+                  onAnswer(question, answer.artist);
+                }}
+              />
+              <label className="artist__name" htmlFor={`answer-${id}`}>
+                <img className="artist__picture" src={answer.picture} alt={answer.artist} />
+                {answer.artist}
+              </label>
+            </div>
+          ))}
         </form>
       </section>
     </section>
   );
 }
-
 export default ArtistQuestionScreen;
