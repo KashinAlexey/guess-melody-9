4ae1514b5ed095448f9cdfd2e8f809e7ca5b391f
Alexey <apple230379@gmail.com>
2022-02-22 20:31:51 +0300

    4.8. Добавит новый компонент GameScreen.  — главный компонент игрового процесса. Он будет управлять показом компонентов  и . Компонент  получает через пропс вопросы - . Поэтому необходимо не забыть указать тип .

--- project/src/const.ts
+++ project/src/const.ts
@@ -12,3 +12,10 @@ export enum AuthorizationStatus {
   NoAuth = 'NO_AUTH',
   Unknown = 'UNKNOWN',
 }
+
+export const FIRST_GAME_STEP = 0;
+
+export enum GameType {
+  Artist = 'artist',
+  Genre = 'genre',
+}
