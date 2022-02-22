615e9eabaaae42489dd98d6207456a81bf22cd89
Alexey <apple230379@gmail.com>
2022-02-22 19:55:41 +0300

    4.4. Рефакторит компонент GenreQuestionScreen. Обновим код компонента. Передадим данные по игре через пропсы и обновим разметку.

--- project/src/components/genre-question-screen/genre-question-screen.tsx
+++ project/src/components/genre-question-screen/genre-question-screen.tsx
@@ -1,17 +1,23 @@
 import Logo from '../logo/logo';
+import {QuestionGenre} from '../../types/question';
 
-function GenreQuestionScreen(): JSX.Element {
+type GenreQuestionScreenProps = {
+  question: QuestionGenre;
+};
+
+function GenreQuestionScreen(props: GenreQuestionScreenProps): JSX.Element {
+  const {question} = props;
+  const {answers, genre} = question;
+
   return (
     <section className="game game--genre">
       <header className="game__header">
         <Logo />
-
         <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
           <circle className="timer__line" cx="390" cy="390" r="370"
             style={{filter: 'url(#blur)', transform: 'rotate(-90deg) scaleY(-1)', transformOrigin: 'center'}}
           />
         </svg>
-
         <div className="game__mistakes">
           <div className="wrong"/>
           <div className="wrong"/>
@@ -20,51 +26,27 @@ function GenreQuestionScreen(): JSX.Element {
       </header>
 
       <section className="game__screen">
-        <h2 className="game__title">Выберите треки</h2>
+        <h2 className="game__title">Выберите {genre} треки</h2>
         <form className="game__tracks">
-          <div className="track">
-            <button className="track__button track__button--play" type="button"></button>
-            <div className="track__status">
-              <audio></audio>
-            </div>
-            <div className="game__answer">
-              <input className="game__input visually-hidden" type="checkbox" name="answer" value="answer-1" id="answer-1" />
-              <label className="game__check" htmlFor="answer-1">Отметить</label>
-            </div>
-          </div>
-
-          <div className="track">
-            <button className="track__button track__button--play" type="button"></button>
-            <div className="track__status">
-              <audio></audio>
-            </div>
-            <div className="game__answer">
-              <input className="game__input visually-hidden" type="checkbox" name="answer" value="answer-2" id="answer-2" />
-              <label className="game__check" htmlFor="answer-2">Отметить</label>
-            </div>
-          </div>
-
-          <div className="track">
-            <button className="track__button track__button--pause" type="button"></button>
-            <div className="track__status">
-              <audio></audio>
-            </div>
-            <div className="game__answer">
-              <input className="game__input visually-hidden" type="checkbox" name="answer" value="answer-3" id="answer-3" />
-              <label className="game__check" htmlFor="answer-3">Отметить</label>
-            </div>
-          </div>
-
-          <div className="track">
-            <button className="track__button track__button--play" type="button"></button>
-            <div className="track__status">
-              <audio></audio>
-            </div>
-            <div className="game__answer">
-              <input className="game__input visually-hidden" type="checkbox" name="answer" value="answer-4" id="answer-4" />
-              <label className="game__check" htmlFor="answer-4">Отметить</label>
-            </div>
-          </div>
+          {answers.map((answer, id) => {
+            const keyValue = `${id}-${answer.src}`;
+            return (
+              <div key={keyValue} className="track">
+                <button className="track__button track__button--play" type="button"/>
+                <div className="track__status">
+                  <audio
+                    src={answer.src}
+                  />
+                </div>
+                <div className="game__answer">
+                  <input className="game__input visually-hidden" type="checkbox" name="answer" value={`answer-${id}`}
+                    id={`answer-${id}`}
+                  />
+                  <label className="game__check" htmlFor={`answer-${id}`}>Отметить</label>
+                </div>
+              </div>
+            );
+          })}
 
           <button className="game__submit button" type="submit">Ответить</button>
         </form>
@@ -72,5 +54,4 @@ function GenreQuestionScreen(): JSX.Element {
     </section>
   );
 }
-
 export default GenreQuestionScreen;
