6d5a703b853d33af854254972bc70f1470b61c19
Alexey <apple230379@gmail.com>
2022-03-09 17:51:12 +0300

    5.4. Перенесёт контроль за состоянием плеера в родительский компонент. Мелодии проигрываются, но есть сложности с управлением воспроизведением. Например, в игре «Угадай жанр» можно одновременно запустить несколько мелодий.
    Перенос контроля за состоянием плеера в родительские компоненты поможет
    решить эту проблему. Проведём небольшой рефакторинг
    и .
    
    В первом компоненте определим стейт с одним полем: . А для
     добавим в стейт поле . В нём
    будем фиксировать плеер, который воспроизводит мелодию в данный момент.
    
    В компоненте  удалим из стейта поле . Больше оно
    нам не потребуется.

--- project/src/components/genre-question-screen/genre-question-screen.tsx
+++ project/src/components/genre-question-screen/genre-question-screen.tsx
@@ -13,6 +13,8 @@ function GenreQuestionScreen(props: GenreQuestionScreenProps): JSX.Element {
   const {answers, genre} = question;
 
   const [userAnswers, setUserAnswers] = useState([false, false, false, false]);
+  const [activePlayer, setActivePlayer] = useState(0);
+
   return (
     <section className="game game--genre">
       <header className="game__header">
@@ -43,8 +45,9 @@ function GenreQuestionScreen(props: GenreQuestionScreenProps): JSX.Element {
             return (
               <div key={keyValue} className="track">
                 <AudioPlayer
-                  autoPlay={id === 0}
+                  isPlaying={id === activePlayer}
                   src={answer.src}
+                  onPlayButtonClick = {() => setActivePlayer(activePlayer === id ? -1 : id)}
                 />
                 <div className="game__answer">
                   <input className="game__input visually-hidden" type="checkbox" name="answer" value={`answer-${id}`}
