2b7579b59bd3bb38f78c3721a12922872544026f
Alexey <apple230379@gmail.com>
2022-03-14 19:30:00 +0300

    6.2. Подключит redux к проекту. Следующим шагом подключим  к проекту.  Создадим новый модуль  в директории . Нам потребуется функция из пакета . С её помощью мы инициализируем хранилище.
    В файле  (точка входа) воспользуемся  из пакета
    . С помощью этого пакета мы настроим связь между React и
    Redux. Обёрнём компонентом  компонент .

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import App from './components/app/app';
 import {questions} from './mocks/questions';
+import {store} from './store';
 
 const Setting = {
   ERRORS_COUNT: 3,
 };
 ReactDOM.render(
   <React.StrictMode>
-    <App
-      errorsCount = {Setting.ERRORS_COUNT}
-      questions = {questions}
-    />
+    <Provider store = {store}>
+      <App
+        errorsCount = {Setting.ERRORS_COUNT}
+        questions = {questions}
+      />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
