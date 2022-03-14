db0c6eaccd30d0fe54dcfb8507786f5a1fca8b1c
Alexey <apple230379@gmail.com>
2022-03-14 19:39:15 +0300

    6.4. Подключит компоненты к глобальному хранилищу. Подключим компоненты  и  к хранилищу. В компоненте  избавимся от внутреннего состояния. Все необходимые данные будем брать из глобального хранилища.
    Для работы с хранилищем воспользуемся хуками  и
    . Чтобы каждый раз не типизировать запросы к стору,
    создадим типизированные версии хуков. Будем использовать их для работы
    со стором.

--- project/src/components/game-screen/game-screen.tsx
+++ project/src/components/game-screen/game-screen.tsx
@@ -1,6 +1,7 @@
-import {useState} from 'react';
 import {Navigate} from 'react-router-dom';
-import {AppRoute, GameType, FIRST_GAME_STEP} from '../../const';
+import {useAppDispatch, useAppSelector} from '../../hooks/';
+import {incrementStep} from '../../store/action';
+import {AppRoute, GameType} from '../../const';
 import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
 import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
 import {QuestionArtist, QuestionGenre, Questions} from '../../types/question';
@@ -14,21 +15,22 @@ type GameScreenProps = {
 };
 
 function GameScreen({questions}: GameScreenProps): JSX.Element {
-  const [step, setStep] = useState(FIRST_GAME_STEP);
+  const step = useAppSelector((state) => state.step);
 
   const question = questions[step];
+
+  const dispatch = useAppDispatch();
 
   if (step >= questions.length || !question) {
     return <Navigate to={AppRoute.Root} />;
   }
-
   switch (question.type) {
     case GameType.Artist:
       return (
         <ArtistQuestionScreenWrapped
           key={step}
           question={question as QuestionArtist}
-          onAnswer={() => setStep((prevStep) => prevStep + 1)}
+          onAnswer={() => dispatch(incrementStep())}
         />
       );
     case GameType.Genre:
@@ -36,12 +38,11 @@ function GameScreen({questions}: GameScreenProps): JSX.Element {
         <GenreQuestionScreenWrapped
           key={step}
           question={question as QuestionGenre}
-          onAnswer={() => setStep((prevStep) => prevStep + 1)}
+          onAnswer={() => dispatch(incrementStep())}
         />
       );
     default:
       return <Navigate to={AppRoute.Root} />;
   }
 }
-
 export default GameScreen;
