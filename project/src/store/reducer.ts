07087be65f936711e83a3a98582d94bd000a4d93
Alexey <apple230379@gmail.com>
2022-03-15 15:46:09 +0300

    6.9. Добавит действие checkUserAnswer. Следующим шагом создадим новое действие, в котором будем выполнять проверку правильности выбранного ответа и на основании проверки увеличивать счётчик ошибок.

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,23 +1,26 @@
 import {createReducer} from '@reduxjs/toolkit';
-import {incrementStep, resetGame} from './action';
+import {incrementStep, checkUserAnswer, resetGame} from './action';
+import {isAnswerCorrect} from '../game';
 import {FIRST_GAME_STEP} from '../const';
 
 const STEP_COUNT = 1;
-
 const initialState = {
   mistakes: 0,
   step: FIRST_GAME_STEP,
 };
-
 const reducer = createReducer(initialState, (builder) => {
   builder
     .addCase(incrementStep, (state) => {
       state.step = state.step + STEP_COUNT;
+    })
+    .addCase(checkUserAnswer, (state, action) => {
+      const {question, userAnswer} = action.payload;
+
+      state.mistakes += Number(!isAnswerCorrect(question, userAnswer));
     })
     .addCase(resetGame, (state) => {
       state.mistakes = 0;
       state.step = FIRST_GAME_STEP;
     });
 });
-
 export {reducer};
