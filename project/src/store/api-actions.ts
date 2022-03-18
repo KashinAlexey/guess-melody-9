b01cfd9efc593edce24ae0388b52585d91215d5f
Alexey <apple230379@gmail.com>
2022-03-18 15:10:18 +0300

    7.9. Добавит асинхронные действия. Создадим отдельный модуль в котором опишем асинхронные действия. В этих действиях будем выполнять запросы к серверу.
    На данном этапе нам потребуются следующие действия: для загрузки списка
    вопросов, проверки наличия авторизации и отправки данных для прохождения
    аутентификации, отправки запроса на выход из приложения.

---
+++ project/src/store/api-actions.ts
@@ -0,0 +1,43 @@
+import {createAsyncThunk} from '@reduxjs/toolkit';
+import {api} from '../store';
+import {store} from '../store';
+import {Questions} from '../types/question';
+import {loadQuestions, requireAuthorization} from './action';
+import {saveToken, dropToken} from '../services/token';
+import {APIRoute, AuthorizationStatus} from '../const';
+import {AuthData} from '../types/auth-data';
+import {UserData} from '../types/user-data';
+
+export const fetchQuestionAction = createAsyncThunk(
+  'data/fetchQuestions',
+  async () => {
+    const {data} = await api.get<Questions>(APIRoute.Questions);
+    store.dispatch(loadQuestions(data));
+  },
+);
+
+export const checkAuthAction = createAsyncThunk(
+  'user/checkAuth',
+  async () => {
+    await api.get(APIRoute.Login);
+    store.dispatch(requireAuthorization(AuthorizationStatus.Auth));
+  },
+);
+
+export const loginAction = createAsyncThunk(
+  'user/login',
+  async ({login: email, password}: AuthData) => {
+    const {data: {token}} = await api.post<UserData>(APIRoute.Login, {email, password});
+    saveToken(token);
+    store.dispatch(requireAuthorization(AuthorizationStatus.Auth));
+  },
+);
+
+export const logoutAction = createAsyncThunk(
+  'user/logout',
+  async () => {
+    await api.delete(APIRoute.Logout);
+    dropToken();
+    store.dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
+  },
+);
