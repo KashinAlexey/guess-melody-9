92c0513cdf9ba780ef9b8f87b63398e5eeb67ba3
Alexey <apple230379@gmail.com>
2022-03-22 21:26:20 +0300

    8.4. Рефакторит компонент GenreQuestionScreen. Отрефакторим компонент . Удалим JSX, который отвечает за вывод вариантов ответов с плеером и подключим компонент .

--- project/src/components/genre-question-screen/genre-question-screen.tsx
+++ project/src/components/genre-question-screen/genre-question-screen.tsx
@@ -1,7 +1,8 @@
-import {FormEvent, ChangeEvent, PropsWithChildren} from 'react';
+import {FormEvent, PropsWithChildren} from 'react';
 import Logo from '../logo/logo';
 import {useUserAnswers} from '../../hooks/use-user-answers';
 import {QuestionGenre, UserGenreQuestionAnswer} from '../../types/question';
+import GenreQuestionItem from '../genre-question-item/genre-question-item';
 
 type GenreQuestionScreenProps = PropsWithChildren<{
   question: QuestionGenre;
@@ -40,20 +41,14 @@ function GenreQuestionScreen(props: GenreQuestionScreenProps): JSX.Element {
           {answers.map((answer, id) => {
             const keyValue = `${id}-${answer.src}`;
             return (
-              <div key={keyValue} className="track">
-                {renderPlayer(answer.src, id)}
-                <div className="game__answer">
-                  <input className="game__input visually-hidden" type="checkbox" name="answer" value={`answer-${id}`}
-                    id={`answer-${id}`}
-                    checked={userAnswers[id]}
-                    onChange={({target}: ChangeEvent<HTMLInputElement>) => {
-                      const value = target.checked;
-                      handleAnswerChange(id, value);
-                    }}
-                  />
-                  <label className="game__check" htmlFor={`answer-${id}`}>Отметить</label>
-                </div>
-              </div>
+              <GenreQuestionItem
+                answer={answer}
+                id={id}
+                key={keyValue}
+                onChange={handleAnswerChange}
+                renderPlayer={renderPlayer}
+                userAnswer={userAnswers[id]}
+              />
             );
           })}
           <button className="game__submit button" type="submit">Ответить</button>
